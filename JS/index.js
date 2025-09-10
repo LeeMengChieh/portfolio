6c61d4c875b16c49c41784caddc50da5193eefd1
LeeMengChieh <aphmiku@gmail.com>
2025-09-10 23:57:16 +0800

    電腦版banner動畫與手機板aboutme區塊完成

--- JS/index.js
+++ JS/index.js
@@ -8,4 +8,21 @@ toggler.addEventListener('click', () => {
 });
 
 // ====================================================banner
+const paths = [...document.querySelectorAll("path")];
+const svg = document.querySelector("svg");
+
+const tl = gsap.timeline({
+    defaults: { duration: 2, ease: "power1.inOut" },
+    onComplete: () => {
+        svg.classList.add("hide");
+    }
+});
+
+paths.forEach((path) => {
+    const length = path.getTotalLength();
+    path.style.strokeDasharray = length;
+    path.style.strokeDashoffset = length;
+    // 把動畫加進 timeline，會自動一個接一個播
+    tl.to(path, { strokeDashoffset: 0 });
+});
 
